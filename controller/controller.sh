c8618551edf1c89750d0be488dc6b8f2747ce4cb
Lluis Santos <lluis@ngynx.com>
2022-04-01 15:25:24 +0200

    feat(wip): wip

--- controller/controller.sh
+++ controller/controller.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-read node
+read askingnode
 
 #CLUSTER_STATUS="ok"
 NODES=()
 while read node;do
-	NODE=${STACK_NAME}_$node
-	NODES+=($NODE:4567)
+  if [[ "$askingnode" != "$node" ]];then
+	  NODES+=($node:4567)
+	fi
   #echo "SHOW GLOBAL STATUS LIKE 'wsrep_%';" | mysql -u $MYSQL_USER $MYSQL_DATABASE -p$MYSQL_PASSWORD -h "galera-$node"
 done < <(docker service ps --filter "desired-state=running" --format "{{.Node}}" ${STACK_NAME}_node)
 
