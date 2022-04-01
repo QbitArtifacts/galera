77d2fe07ca333ff36dda0e1206f6f76131a244ae
Lluis Santos <lluis@ngynx.com>
2022-04-01 15:06:55 +0200

    feat(wip): wip

--- controller/controller.sh
+++ controller/controller.sh
@@ -3,8 +3,14 @@
 read node
 
 #CLUSTER_STATUS="ok"
-#docker service ps --filter "desired-state=running" --format "{{.Node}}" galera-stage_node | while read node;do
+NODES=()
+while read node;do
+	NODE=${STACK_NAME}_$node
+	NODES+=($NODE:4567)
   #echo "SHOW GLOBAL STATUS LIKE 'wsrep_%';" | mysql -u $MYSQL_USER $MYSQL_DATABASE -p$MYSQL_PASSWORD -h "galera-$node"
-#done
+done < <(docker service ps --filter "desired-state=running" --format "{{.Node}}" ${STACK_NAME}_node)
 
-echo "--wsrep_cluster_address='gcomm://'"
+JOIN_NODES=$(echo ${NODES[*]} | tr ' ' ',')
+COMMAND="--wsrep_cluster_address='gcomm://$JOIN_NODES'"
+
+echo $COMMAND
--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-nodes=$(echo "list_nodes" | nc controller 80)
+command=$(echo $HOSTNAME | nc controller 80)
 
-mysqld --user=root --wsrep-cluster-address="gcomm://$nodes:4567"
+mysqld --user=root $command
